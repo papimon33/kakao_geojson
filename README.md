7cc54ce50f34bc2d42367281494eb01e639a7628
Jake <jakepark@airport.co.kr>
2025-03-24 15:59:22 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,91 @@
+# Getting Started with Create React App
+
+This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+
+## Available Scripts
+
+In the project directory, you can run:
+
+### `npm start`
+
+Runs the app in the development mode.\
+Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
+
+The page will reload when you make changes.\
+You may also see any lint errors in the console.
+
+### `npm test`
+
+Launches the test runner in the interactive watch mode.\
+See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
+
+### `npm run build`
+
+Builds the app for production to the `build` folder.\
+It correctly bundles React in production mode and optimizes the build for the best performance.
+
+The build is minified and the filenames include the hashes.\
+Your app is ready to be deployed!
+
+See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
+
+### `npm run eject`
+
+**Note: this is a one-way operation. Once you `eject`, you can't go back!**
+
+If you aren't satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
+
+Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you're on your own.
+
+You don't have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn't feel obligated to use this feature. However we understand that this tool wouldn't be useful if you couldn't customize it when you are ready for it.
+
+## Learn More
+
+You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
+
+To learn React, check out the [React documentation](https://reactjs.org/).
+
+### Code Splitting
+
+This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
+
+### Analyzing the Bundle Size
+
+This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
+
+### Making a Progressive Web App
+
+This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
+
+### Advanced Configuration
+
+This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
+
+### Deployment
+
+This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
+
+### `npm run build` fails to minify
+
+This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+
+# GeoJSON 파일 결합기
+
+GeoJSON 파일들을 드래그 앤 드롭으로 업로드하고 하나의 파일로 결합할 수 있는 웹 애플리케이션입니다.
+
+## 기능
+
+- GeoJSON 파일 드래그 앤 드롭 업로드
+- 파일 순서 변경 가능
+- 여러 GeoJSON 파일을 하나로 결합
+- properties.id를 최상위 레벨로 이동
+
+## 사용 방법
+
+1. GeoJSON 파일을 드래그 앤 드롭 영역에 업로드합니다.
+2. 필요한 경우 파일 순서를 드래그하여 변경합니다.
+3. '결합' 버튼을 클릭하여 하나의 GeoJSON 파일로 다운로드 받습니다.
+
+## 배포된 사이트
+
+https://{your-github-username}.github.io/{repository-name}
---
+++ src/App.js
@@ -0,0 +1,152 @@
+import React, { useState } from 'react';
+import { DragDropContext, Droppable, Draggable } from '@hello-pangea/dnd';
+import styled from 'styled-components';
+
+const DropZone = styled.div`
+  border: 2px dashed #cccccc;
+  border-radius: 4px;
+  padding: 20px;
+  text-align: center;
+  margin: 20px;
+  min-height: 200px;
+`;
+
+const FileList = styled.div`
+  margin: 20px;
+`;
+
+const FileItem = styled.div`
+  padding: 10px;
+  margin: 5px 0;
+  background-color: #f5f5f5;
+  border-radius: 4px;
+`;
+
+const MergeButton = styled.button`
+  padding: 10px 20px;
+  background-color: #4CAF50;
+  color: white;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+  margin: 20px;
+  
+  &:hover {
+    background-color: #45a049;
+  }
+`;
+
+function App() {
+  const [files, setFiles] = useState([]);
+
+  const handleDragOver = (event) => {
+    event.preventDefault();
+  };
+
+  const handleDrop = async (event) => {
+    event.preventDefault();
+    const newFiles = Array.from(event.dataTransfer.files);
+    const jsonFiles = newFiles.filter(file => file.name.endsWith('.json') || file.name.endsWith('.geojson'));
+    
+    const filesWithContent = await Promise.all(
+      jsonFiles.map(async (file) => {
+        const content = await file.text();
+        return {
+          name: file.name,
+          content: JSON.parse(content)
+        };
+      })
+    );
+    
+    setFiles([...files, ...filesWithContent]);
+  };
+
+  const handleDragEnd = (result) => {
+    if (!result.destination) return;
+    
+    const items = Array.from(files);
+    const [reorderedItem] = items.splice(result.source.index, 1);
+    items.splice(result.destination.index, 0, reorderedItem);
+    
+    setFiles(items);
+  };
+
+  const handleMerge = () => {
+    if (files.length === 0) return;
+
+    const mergedFeatures = files.reduce((acc, file) => {
+      return [...acc, ...file.content.features];
+    }, []);
+
+    const resultGeoJson = {
+      type: "FeatureCollection",
+      features: mergedFeatures.map((feature) => {
+        const newFeature = {};
+        
+        if (feature.properties && feature.properties.id) {
+          newFeature.id = feature.properties.id;
+          const { id, ...restProperties } = feature.properties;
+          feature.properties = restProperties;
+        }
+
+        return {
+          ...newFeature,
+          ...feature
+        };
+      })
+    };
+
+    const blob = new Blob([JSON.stringify(resultGeoJson, null, 2)], { type: 'application/json' });
+    const url = URL.createObjectURL(blob);
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = 'result.geojson';
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+    URL.revokeObjectURL(url);
+  };
+
+  return (
+    <div>
+      <DropZone
+        onDragOver={handleDragOver}
+        onDrop={handleDrop}
+      >
+        GeoJSON 파일을 여기에 드래그 앤 드롭하세요
+      </DropZone>
+
+      <DragDropContext onDragEnd={handleDragEnd}>
+        <Droppable droppableId="files">
+          {(provided) => (
+            <FileList
+              {...provided.droppableProps}
+              ref={provided.innerRef}
+            >
+              {files.map((file, index) => (
+                <Draggable key={file.name} draggableId={file.name} index={index}>
+                  {(provided) => (
+                    <FileItem
+                      ref={provided.innerRef}
+                      {...provided.draggableProps}
+                      {...provided.dragHandleProps}
+                    >
+                      {file.name}
+                    </FileItem>
+                  )}
+                </Draggable>
+              ))}
+              {provided.placeholder}
+            </FileList>
+          )}
+        </Droppable>
+      </DragDropContext>
+
+      <MergeButton onClick={handleMerge}>
+        결합
+      </MergeButton>
+    </div>
+  );
+}
+
+export default App;
